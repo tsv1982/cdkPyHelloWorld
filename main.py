4e61cc7962bf353dd14285928101dc0663f43509
Tarabanov Serhii <tsv198250@gmail.com>
2022-09-13 21:39:49 +0300

    001

--- main.py
+++ main.py
@@ -6,38 +6,38 @@ app = Flask(__name__)
 import psycopg2
 
 
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(dbname=os.environ["DB_NAME"], user=os.environ["DB_USER"],
-                                password=os.environ["DB_PASS"], host=os.environ["DB_HOST"], port="5432")
-
-        # create a cursor
-        cur = conn.cursor()
-
-        # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-
-        # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
-
-
-if __name__ == '__main__':
-    connect()
+# def connect():
+#     """ Connect to the PostgreSQL database server """
+#     conn = None
+#     try:
+#         # connect to the PostgreSQL server
+#         print('Connecting to the PostgreSQL database...')
+#         conn = psycopg2.connect(dbname=os.environ["DB_NAME"], user=os.environ["DB_USER"],
+#                                 password=os.environ["DB_PASS"], host=os.environ["DB_HOST"], port="5432")
+#
+#         # create a cursor
+#         cur = conn.cursor()
+#
+#         # execute a statement
+#         print('PostgreSQL database version:')
+#         cur.execute('SELECT version()')
+#
+#         # display the PostgreSQL database server version
+#         db_version = cur.fetchone()
+#         print(db_version)
+#
+#         # close the communication with the PostgreSQL
+#         cur.close()
+#     except (Exception, psycopg2.DatabaseError) as error:
+#         print(error)
+#     finally:
+#         if conn is not None:
+#             conn.close()
+#             print('Database connection closed.')
+#
+#
+# if __name__ == '__main__':
+#     connect()
 
 
 @app.route('/')
